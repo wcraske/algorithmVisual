15439d643f89e74bb6eb1e2284d67dd13ed0d006
Angad Kahlon <Angad.Kahlon@student.ufv.ca>
2024-03-01 17:54:37 -0800

    Continued working on the grassfire algorithm, added in the "fire" element of the algorithm (the spreading).

--- Grassfire/grassfire.py
+++ Grassfire/grassfire.py
@@ -10,6 +10,18 @@ def grassfire(graph, start_node):
     arr.append(start_node)
     distance[start_node] = 0
 
+    while arr:
+        current_node = arr.popleft()
+        visited[current_node] = True
+        
+        for n in graph.get(current_node, []):
+            if not visited[n]:
+                arr.append[n]
+                visited[n] = True
+                distance[n] = distance[current_node] + 1
+
+    return distance
+
 
 
 
