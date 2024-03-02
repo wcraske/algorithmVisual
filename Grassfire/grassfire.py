248c15a3d204310bce65759edb185a05ecff818c
Angad Kahlon <Angad.Kahlon@student.ufv.ca>
2024-03-01 20:39:54 -0800

    Last finishing stuff with the implementation of the Graph data structure along with some other code edits that allow the code to return the path and distances from the start node.

--- Grassfire/grassfire.py
+++ Grassfire/grassfire.py
@@ -1,40 +1,51 @@
+from collections import deque
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+
+    def grassfire(self, startNode):
+        visited = {}
+        distance = {}
+        predecessor = {}
+
+        for node in self.graph:
+            visited[node] = False
+            distance[node] = int(-1)
+            predecessor[node] = None
 
 
+        arr = deque([startNode])
+        visited[startNode] = True
+        distance[startNode] = 0
 
-def grassfire(graph, start_node):
-    visited = set()
-    distance = {}
+        while arr:
+            current_node = arr.popleft()
+            
+            for n in self.graph.get(current_node, []):
+                if not visited[n]:
+                    visited[n] = True
+                    arr.append(n)
+                    distance[n] = distance[current_node] + 1
+                    predecessor[n] = current_node                    
 
-    for node in self.graph:
-        visited[node] = False
-        distance[node] = int(-1)
-        predecessor[node] = False
-
-
-    arr = deque([startNode])
-    visited.add(startNode)
-    distance[start_node] = 0
-
-    while arr:
-        current_node = arr.popleft()
+        for x in distance:
+            if distance[x] == -1:
+                distance[x] = "Not possible to reach from start node"
         
-        for n in self.graph.get(current_node, []):
-            if n not in visited:
-                visited.add(n)
-                arr.append[n]
-                distance[n] = distance[current_node] + 1
-                predecessor[n] = current_node
-
-    for x in distance:
-        if distance[x] == -1:
-            distance[x] = "Not possible to reach from start node"
-    
-    return distance, predecessor
+        return distance, predecessor
 
     def reconstruct_path(self, startNode, goalNode, predecessor):
         path = []
         current_node = goalNode
-        while current_node is not False:
+        while current_node is not None:
             path.append(current_node)
             current_node = predecessor[current_node]
         path.reverse()
@@ -43,24 +54,25 @@ def grassfire(graph, start_node):
         else:
             return "No path from start node to goal node!"
 
-graph = Graph()
-print("\n")
-print("Welcome to the Grassfire Algorithm Implementation on a Graph Data Structure!")
-print("Important Info: The start node is always 0!")
-print("\n")
-num = int(input("Enter in the number of edges in the graph: "))
-print("\n")
-while num > 0:
-    vertex1 = int(input("Enter in the first vertex of the edge: "))
-    vertex2 = int(input("Enter in the second vertex of the edge: "))
+if __name__ == "__main__":
+    graph = Graph()
     print("\n")
-    graph.addEdge(vertex1, vertex2)
-    num = num - 1
-startNode = 0
-distances = graph.grassfire(startNode)
-dis = distances[0]
-predes = distances[1]
-print("Distances from start node", startNode, ":", dis) 
-goalNode = int(input("Enter in the goal vertex number: "))
-path = graph.reconstruct_path(startNode, goalNode, predes)
-print("Path from start node", startNode, "to node", goalNode, ":", path)
+    print("Welcome to the Grassfire Algorithm Implementation on a Graph Data Structure!")
+    print("Important Info: The start node is always 0!")
+    print("\n")
+    num = int(input("Enter in the number of edges in the graph: "))
+    print("\n")
+    while num > 0:
+        vertex1 = int(input("Enter in the first vertex of the edge: "))
+        vertex2 = int(input("Enter in the second vertex of the edge: "))
+        print("\n")
+        graph.add_edge(vertex1, vertex2)
+        num = num - 1
+    startNode = 0
+    distances = graph.grassfire(startNode)
+    dis = distances[0]
+    predes = distances[1]
+    print("Distances from start node", startNode, ":", dis) 
+    goalNode = int(input("Enter in the goal vertex number: "))
+    path = graph.reconstruct_path(startNode, goalNode, predes)
+    print("Path from start node", startNode, "to node", goalNode, ":", path)
